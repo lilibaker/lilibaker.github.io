34bbb36f22f6c77259351c2980c358590a33399d
Lili Baker <lilijbaker@gmail.com>
2022-09-15 20:37:50 -0400

    Add partial contact form

---
+++ contact.php
@@ -0,0 +1,51 @@
+<?php
+
+if($_POST) {
+    $name = "";
+    $email = "";
+    $message = "";
+    $email_body = "<div>";
+
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$name."</span>
+                        </div>";
+    }
+
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+    }
+
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$message."</div>
+                        </div>";
+    }
+
+    $recipient = "ljbaker.contact@gmail.com";
+
+    $subject = "Website request";
+
+    $email_body .= "</div>";
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+
+    if(mail($recipient, $subject, $email_body, $headers)) {
+        echo "<p>Thank you for contacting us, $name.";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+
+} else {
+    echo '<p>Something went wrong</p>';
+}
+?>
